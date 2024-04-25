283c0ed77d9e18fd9836d44e77165d6561cce06b
oosakiharuki <k023g0035@g.neec.ac.jp>
2024-04-25 19:40:18 +0900

    transformedもうまく作れた

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <Novice.h>
+#include<assert.h>
 
 const char kWindowTitle[] = "LC1B_03_オオサキ_ハルキ_タイトル";
 
@@ -67,9 +68,14 @@ Matrix4x4 MakeScaleMatrix(const Vector3& scale) {
 Vector3 Transform(const Vector3& vector, const Matrix4x4& matrix) {
 	Vector3 result;
 
-	result.x = matrix.m[0][0] * vector.x / matrix.m[3][3];
-	result.y = matrix.m[1][1] * vector.y / matrix.m[3][3];
-	result.z = matrix.m[2][2] * vector.z / matrix.m[3][3];
+	result.x = vector.x * matrix.m[0][0] + vector.y * matrix.m[1][0] + vector.z * matrix.m[2][0] + 1.0f * matrix.m[3][0];
+	result.y = vector.x * matrix.m[0][1] + vector.y * matrix.m[1][1] + vector.z * matrix.m[2][1] + 1.0f * matrix.m[3][1];
+	result.z = vector.x * matrix.m[0][2] + vector.y * matrix.m[1][2] + vector.z * matrix.m[2][2] + 1.0f * matrix.m[3][2];
+	float w = vector.x * matrix.m[0][3] + vector.y * matrix.m[1][3] + vector.z * matrix.m[2][3] + 1.0f * matrix.m[3][3];
+	assert(w != 0.0f);
+	result.x /= w;
+	result.y /= w;
+	result.z /= w;
 
 	return result;
 }
